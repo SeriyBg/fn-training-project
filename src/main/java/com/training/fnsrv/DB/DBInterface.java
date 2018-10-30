a2cc2d0b1718cfd7e0800d203d5a2cb6a5a2fb87
Oleksii Kolka <avesat@gmail.com>
2018-10-30 18:56:27 +0200

    Implemented interface for Data Base, Memory Data Base and Service

---
+++ src/main/java/com/training/fnsrv/DB/DBInterface.java
@@ -0,0 +1,18 @@
+package com.training.fnsrv.DB;
+
+import com.training.fnsrv.model.Host;
+import com.training.fnsrv.model.IpInterface;
+import com.training.fnsrv.model.IpRoute;
+
+import java.util.List;
+
+public interface DBInterface {
+    void saveHost(Host host);
+    Host getHostById(Long id);
+
+    void saveIpInterface(IpInterface ipInterface);
+    List<IpInterface> getAllIpInterfacesById(Long id);
+
+    void saveIpRoute(IpRoute ipRoute);
+    List<IpRoute> getAllIpRoutesById(Long id);
+}
---
+++ src/main/java/com/training/fnsrv/service/DBService.java
@@ -0,0 +1,44 @@
+package com.training.fnsrv.service;
+
+import com.training.fnsrv.DB.MemoryDB;
+import com.training.fnsrv.model.Host;
+import com.training.fnsrv.model.IpInterface;
+import com.training.fnsrv.model.IpRoute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DBService {
+    @Autowired
+    MemoryDB db;
+
+    public void saveHost(Host host) {
+        db.saveHost(host);
+    }
+
+    public Host getHostById(Long id) {
+        return db.getHostById(id);
+    }
+
+    public void saveIpInterface(IpInterface ipInterface) {
+        db.saveIpInterface(ipInterface);
+    }
+
+    public List<IpInterface> getAllIpInterfacesById(Long id) {
+        return db.getAllIpInterfacesById(id);
+    }
+
+    public IpInterface getIpInterfacesByIdAndName(Long id, String name) {
+        return db.getIpInterfacesByIdAndName(id, name);
+    }
+
+    public void saveIpRoute(IpRoute ipRoute) {
+        db.saveIpRoute(ipRoute);
+    }
+
+    public List<IpRoute> getAllIpRoutesById(Long id){
+        return db.getAllIpRoutesById(id);
+    }
+}

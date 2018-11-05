7dd0a05e7962b3fefb3f53acc1e180acdefb886d
Oleksii Kolka <avesat@gmail.com>
2018-11-05 18:08:12 +0200

    Implemented ProtoBuf serialization/deserialization for Host model

--- src/main/java/com/training/fnsrv/service/HostService.java
+++ src/main/java/com/training/fnsrv/service/HostService.java
@@ -1,7 +1,10 @@
 package com.training.fnsrv.service;
 
+import com.google.gson.Gson;
+import com.google.protobuf.util.JsonFormat;
 import com.training.fnsrv.dao.InMemoryHostDao;
 import com.training.fnsrv.model.*;
+import com.training.fnsrv.protobuff.gen.HostProto;
 import com.training.fnsrv.task.HostTask;
 import com.training.fnsrv.task.TaskExecutor;
 import lombok.Getter;
@@ -54,6 +57,98 @@ public class HostService {
         for (Host host : hostDao.findAll()) {
             hosts.add(host);
         }
+
         return hosts;
+    }
+
+    public HostProto.Host protoSerializeById(Long id) {
+        HostProto.Host.Builder hostProto = HostProto.Host.newBuilder();
+
+        //TODO: Is it possible copy Host model directly to protoHost?
+        for (Host host : hostDao.findAll()) {
+            if (host.getRequestId() == id) {
+                hostProto
+                        .setId(host.getId())
+                        .setRequestId(host.getRequestId())
+                        .setIpAddress(host.getIpAddress())
+                        .setUser(host.getUser())
+                        .setPassword(host.getPassword());
+
+                for (IpInterface intf : host.getIpInterfaces()) {
+                    hostProto.addIpInterfaces(HostProto.Host.IpInterface.newBuilder()
+                            .setId(intf.getId())
+                            .setName(intf.getName())
+                            .setMacAddress(intf.getMacAddress())
+                            .setIpAddress(HostProto.Host.IpAddress.newBuilder()
+                                    .setId(intf.getIpAddress().getId())
+                                    .setAddr(intf.getIpAddress().getAddr())
+                                    .setNetmask(intf.getIpAddress().getNetmask())
+                                    .build())
+                            .setMtu(intf.getMtu())
+                            .build()
+                    );
+                }
+
+                for (IpRoute route : host.getIpRoutes()) {
+                    /* Find interface which was added to hostProto */
+                    HostProto.Host.IpInterface ipInterface = null;
+                    for (HostProto.Host.IpInterface intf : hostProto.getIpInterfacesList()) {
+                        if (intf.getId() == route.getIface().getId()) {
+                            ipInterface = intf;
+                        }
+                    }
+                    if (ipInterface == null) {
+                        ipInterface = HostProto.Host.IpInterface.newBuilder().build();
+                    }
+
+                    hostProto.addIpRoutes(HostProto.Host.IpRoute.newBuilder()
+                            .setId(route.getId())
+                            .setDestination(route.getDestination())
+                            .setGateway(route.getGateway())
+                            .setGenmask(route.getGenmask())
+                            .setFlags(route.getFlags())
+                            .setMetric(route.getMetric())
+                            .setRefs(route.getRefs())
+                            .setIface(ipInterface)
+                            .build()
+                    );
+                }
+                break;
+            }
+        }
+
+        return hostProto.build();
+    }
+
+    public HostProto.Host protoSerializeViaJsonById(Long id) {
+        HostProto.Host.Builder hostProto = HostProto.Host.newBuilder();
+
+        for (Host host : hostDao.findAll()) {
+            if (host.getRequestId() == id) {
+                try {
+                    JsonFormat.parser().merge((new Gson()).toJson(host), hostProto);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+
+        return hostProto.build();
+    }
+
+    public Host protoDeserialize(HostProto.Host hostProto) {
+        JsonFormat.Printer jsonFormat = JsonFormat.printer();
+        String hostProtoJson = null;
+
+        try {
+            hostProtoJson = jsonFormat.print(hostProto);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return (new Gson()).fromJson(hostProtoJson, Host.class);
     }
 }
